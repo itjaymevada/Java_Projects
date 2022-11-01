8cc37db980ba9e5b557420c52c6aa2e475311da4
Jay Mevada <89245561+itjaymevada@users.noreply.github.com>
2022-11-01 15:15:28 +0530

    Add files via upload

---
+++ digital_clock/src/com/digi_clock/Main.java
@@ -0,0 +1,9 @@
+package com.digi_clock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        MyWindow w = new MyWindow();
+    }
+}
